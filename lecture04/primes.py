5be9a0887685664713c068505560245b929e9c87
scaredyneko <cat@satoko.org>
2020-10-18 20:29:29 +0000

    rework and completion

--- lecture04/primes.py
+++ lecture04/primes.py
@@ -1,29 +1,52 @@
-# taking the value of n and checking whether it fits the criteria
-while True:
-    n = input("Give me an integer greater than 2: ")
-    
-    if not n.isnumeric():
-        print("It's not a number.")
-        continue
-    else:
-        n = int(n)
-        if n < 3:
-            print("It's not in the range.")
-            continue
-    
-# a list to store composite numbers
-excluded = []
+# получение значения n и проверка на наличие ошибок
+appropriate = False
 
-# sieve of eratosthenes algorithm
-for element in range(2, n):
-    for number in range(element + 1, n):
-        if number % element == 0:
-            if number not in excluded: 
-                excluded.append(number)
-    # printing the prime to the file
-    if element not in excluded:
-        if element == 2:
-            with open('primes.txt', 'w'):
-                pass
-        with open('primes.txt', 'a+') as primes:
-            primes.write(str(element) + '\n')
+while not appropriate:
+    n = input('Give me a number greater than 2: ')
+    n = n.strip()
+    n = n.replace(',', '.')
+    
+    for char in n:
+        if char not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+            if char != '.' or '.' not in n[1:] or len(n) < 3 or n.count('.') > 1:
+                print('Try again.')
+                break
+        if char == n[-1]:
+            appropriate = True
+                
+    if appropriate:
+        n = int(n[:n.find('.') if n.count('.') == 1 else len(n)])
+        
+        if n < 2:
+            print('Try again.')
+            appropriate = False
+        
+
+# создание нового файла и/или удаление из него содержимого
+with open('primes.txt', 'w'):
+    pass
+
+# списки для хранения составных и простых чисел
+composite_numbers = []
+prime_numbers = list(range(2, n))
+
+# поиск простых чисел с помощью алгоритма "решето эратосфена"
+for element in prime_numbers:
+    if element not in composite_numbers:
+        if element * element >= n:
+            break
+        else:
+            for number in range(element * element, n, element):
+                composite_numbers.append(number)
+                try:
+                    prime_numbers.remove(number)
+                except:  # если у числа несколько простых множителей и оно уже было удалено
+                    continue
+
+# запись простых чисел в файл
+for prime in prime_numbers:
+    with open('primes.txt', 'a') as text_file:
+        text_file.write(str(prime) + '\n')
+        
+# уведомление о завершении работы
+print("Done.")
